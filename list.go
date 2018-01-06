f602c523d207a49d65626e90b6dea4e91cf38bd6
Valeriy Kostikov <mkoctuk@gmail.com>
2018-01-06 12:53:23 +0300

    add length

--- list.go
+++ list.go
@@ -2,28 +2,35 @@ package gopool
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 type taskList struct {
 	sync.RWMutex
 	list []Task
+	len  int64
 }
 
 func (tasks *taskList) put(task Task) {
 	tasks.Lock()
 	tasks.list = append(tasks.list, task)
+	tasks.len++
 	tasks.Unlock()
 }
 
 func (tasks *taskList) get() (Task, bool) {
-	tasks.Lock()
 	var task Task
-	if len(tasks.list) > 0 {
+	if tasks.length() > 0 {
+		tasks.Lock()
 		task = tasks.list[0]
 		tasks.list = tasks.list[1:]
+		tasks.len--
 		tasks.Unlock()
 		return task, true
 	}
-	tasks.Unlock()
 	return task, false
+}
+
+func (tasks *taskList) length() int64 {
+	return atomic.LoadInt64(&tasks.len)
 }
